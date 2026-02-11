8e1abfe274c520f97e0e2714518d3585dfdc028a
Gustavo Costa <gustavocosta.miguel@estudante.ufjf.br>
2026-02-11 12:21:35 -0300

    Aprende separação,  type e conversão

---
+++ aula1.py
@@ -0,0 +1,9 @@
+#Primeiro comentário em python
+print(456)
+"""
+A DOCSTRING
+"""
+print(23,41,14,sep="-",end="\n^^") #em python ao usar virgula, o print imprime os valores separados por espaço
+#usamos sep para escolher o tipo do espaçamento e end para escolher o que vai ser impresso no final
+print('"Coloquei aspas duplas"') 
+print('estou', 'testando"aspas"','a')
---
+++ aula4.py
@@ -0,0 +1,4 @@
+print(2.45,type(200) ) #Type diz o tipo da variavel
+print(22=="22")
+print(22=="22",22==22.0) #teste de igualdade
+print(int('1')+ 1,bool(2)) #converte string para inteiro
